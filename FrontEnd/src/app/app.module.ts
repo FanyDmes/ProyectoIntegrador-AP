4d8a7bf266142cd57773859aee44717724c0c184
FanyDmes <95221534+FanyDmes@users.noreply.github.com>
2022-07-21 16:28:47 -0300

    educacion

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './components/home/home.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LogoAPComponent} from './components/logo-ap/logo-ap.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { LogoAPComponent} from './components/logo-ap/logo-ap.component';
     BannerComponent,
     HomeComponent,
     AcercaDeComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    EducacionComponent
   
   ],
   imports: [
