c7c4a74b0bce7fa8196fa1f7f74da6d1b88b6b38
FanyDmes <95221534+FanyDmes@users.noreply.github.com>
2022-07-25 05:50:51 -0300

    new-educacion

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -5,46 +5,47 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { BannerComponent } from './components/banner/banner.component';
-import { HomeComponent } from './components/home/home.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { HttpClientModule } from '@angular/common/http';
-import { LogoAPComponent} from './components/logo-ap/logo-ap.component';
-import { EducacionComponent } from './components/educacion/educacion.component';
-import { LoginComponent } from './components/login/login.component';
-import { interceptorProvider } from './service/interceptor-service';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
-import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
-import { EditExperienciaComponent } from './components/experiencia/edit-experiencia/edit-experiencia.component';
-import { HysComponent } from './components/hys/hys.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent,
-    HomeComponent,
-    AcercaDeComponent,
     LogoAPComponent,
-    EducacionComponent,
-    LoginComponent,
+    BannerComponent,
+    AcercaDeComponent,
     ExperienciaComponent,
-    NewExperienciaComponent,
-    EditExperienciaComponent,
+    EducacionComponent,
     HysComponent,
     ProyectoComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpClientModule,
     NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
   providers: [
     interceptorProvider
